05ea9e92b2392d83e7749eba6583a88e37c1c7ee
mssdfg0708 <mssdfg0708@naver.com>
2021-09-16 23:36:31 +0900

    Solve BaekJoon 1010

---
+++ BaekJoon/1010번 다리 놓기.py
@@ -0,0 +1,17 @@
+import sys
+
+t = int(input())
+for _ in range(t):
+    n, m = map(int, sys.stdin.readline().split())
+    dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if i == 1:
+                dp[i][j] = j
+                continue
+            if i == j:
+                dp[i][j] = 1
+            elif j > i:
+                dp[i][j] = dp[i][j-1] + dp[i-1][j-1]
+
+    print(dp[n][m])
