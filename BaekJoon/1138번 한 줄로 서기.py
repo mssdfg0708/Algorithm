071f770783d4d7288e313a4cd2ccada4fca57e9a
mssdfg0708 <mssdfg0708@naver.com>
2022-05-10 22:16:35 +0900

    Solve BaekJoon 1138

---
+++ BaekJoon/1138번 한 줄로 서기.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+answer = [0] * n
+
+for i in range(n):
+    cnt = 0
+    for j in range(n):
+        if cnt == arr[i] and answer[j] == 0:
+            answer[j] = i + 1
+            break
+        elif answer[j] == 0:
+            cnt += 1
+
+for item in answer:
+    print(item, end=' ')
