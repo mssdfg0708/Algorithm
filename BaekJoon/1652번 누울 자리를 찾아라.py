99bacbc351484ff08168c0e03ca5bf26c4b95329
mssdfg0708 <mssdfg0708@naver.com>
2022-06-27 19:22:13 +0900

    Solve BaekJoon 1652

---
+++ BaekJoon/1652번 누울 자리를 찾아라.py
@@ -0,0 +1,35 @@
+n = int(input())
+arr = []
+cnt_w = 0
+cnt_h = 0
+position_w = 0
+position_h = 0
+
+for i in range(n):
+    arr.append(input())
+
+for i in arr:
+    for j in i:
+        if j == '.':
+            cnt_w += 1
+        else:
+            if cnt_w > 1:
+                position_w += 1
+            cnt_w = 0
+    if cnt_w > 1:
+        position_w += 1
+    cnt_w = 0
+
+for i in range(n):
+    for j in range(n):
+        if arr[j][i] == '.':
+            cnt_h += 1
+        else:
+            if cnt_h > 1:
+                position_h += 1
+            cnt_h = 0
+    if cnt_h > 1:
+        position_h += 1
+    cnt_h = 0
+
+print(position_w, position_h)
